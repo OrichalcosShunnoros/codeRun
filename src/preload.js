96d75572da6c8616653acc3a4f1e656fed89dbfa
OrichalcosShunnoros <c3146092091@gmail.com>
2025-03-28 02:04:41 -0500

    auste visual code

--- src/preload.js
+++ src/preload.js
@@ -1,6 +1,6 @@
-const { contextBridge } = require("electron");
+const { contextBridge } = require('electron')
 
-contextBridge.exposeInMainWorld("electron", {
-    log: (message) => console.log(message),
-    fetch: (url) => fetch(url).then((res) => res.text()),
-});
+contextBridge.exposeInMainWorld('electron', {
+  log: (message) => console.log(message),
+  fetch: (url) => fetch(url).then((res) => res.text())
+})
