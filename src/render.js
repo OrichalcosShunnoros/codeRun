5e4a604f7b3c2043c6f91551f09f23c34a7d6e6e
OrichalcosShunnoros <c3146092091@gmail.com>
2025-03-28 02:03:18 -0500

    mod

--- src/render.js
+++ src/render.js
@@ -70,21 +70,15 @@ sys.stderr = sys.stdout
     }
 }
 
-
 editor.addEventListener("input", evaluateCode);
 languageSelect.addEventListener("change", evaluateCode);
 
-// Theme toggle functionality
 const themeToggleBtn = document.querySelector("header button");
 
-
-
-// Function to toggle theme
 function toggleTheme() {
     const body = document.body;
     const isLightTheme = body.classList.toggle("light-theme");
 
-    // Definir los iconos SVG como cadenas
     const sunIcon = `<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <circle cx="12" cy="12" r="5" stroke="currentColor" stroke-width="2"/>
         <path d="M12 2v2M12 20v2M4.22 4.22l1.42 1.42M16.36 16.36l1.42 1.42M2 12h2M20 12h2M4.22 19.78l1.42-1.42M16.36 7.64l1.42-1.42" stroke="currentColor" stroke-width="2"/>
@@ -94,23 +88,18 @@ function toggleTheme() {
         <path d="M21 12.79A9 9 0 0 1 11.21 3 7 7 0 1 0 21 12.79z" stroke="currentColor" stroke-width="2"/>
     </svg>`;
 
-    // Cambiar el contenido del botón
     themeToggleBtn.innerHTML = isLightTheme ? `${moonIcon}` : `${sunIcon}`;
 
-    // Guardar la preferencia en localStorage
     localStorage.setItem("theme", isLightTheme ? "light" : "dark");
 }
 
-// Evento de clic en el botón de cambio de tema
 themeToggleBtn.addEventListener("click", toggleTheme);
 
-// Verificar la preferencia guardada al cargar la página
 document.addEventListener("DOMContentLoaded", () => {
     const savedTheme = localStorage.getItem("theme");
 
     if (savedTheme === "light") {
         document.body.classList.add("light-theme");
-        toggleTheme(); // Asegura que el icono también se actualice correctamente
+        toggleTheme(); 
     }
 });
-
